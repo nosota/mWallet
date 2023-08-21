496d4c3e52a47d12da09bd1e1f823709aac0b3d9
ivanvaganov <iv@nosota.com>
2023-08-21 17:19:32 +0300

    Removed command line runner

--- service/src/main/java/com/nosota/mwallet/MwalletApplication.java
+++ service/src/main/java/com/nosota/mwallet/MwalletApplication.java
@@ -1,27 +1,11 @@
 package com.nosota.mwallet;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MwalletApplication implements CommandLineRunner {
-    private static final Logger LOG = LoggerFactory.getLogger(MwalletApplication.class);
-
+public class MwalletApplication {
     public static void main(String[] args) {
-        LOG.info("STARTING THE APPLICATION");
         SpringApplication.run(MwalletApplication.class, args);
-        LOG.info("APPLICATION FINISHED");
-    }
-
-    @Override
-    public void run(String... args) {
-        LOG.info("EXECUTING : command line runner");
-
-        for (int i = 0; i < args.length; ++i) {
-            LOG.info("args[{}]: {}", i, args[i]);
-        }
     }
 }
